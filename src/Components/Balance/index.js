22d5cda416655fd1f411a8809fb5b6991d5ec7f8
Geovanne Callegaro <marcos.martins.ext@dasa.com.br>
2022-08-20 09:41:19 -0300

    add animating in Balance component

--- src/Components/Balance/index.js
+++ src/Components/Balance/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native'
 
+import { MotiView} from 'moti';
+
 export default function Balance({ balance, expenses }) {
     return (
-        <View style={styles.container}>
+        <MotiView 
+            style={styles.container}
+            from={{
+                rotateX: '-100deg',
+                opcaity: 0
+            }}
+            animate={{
+                rotateX: '0deg',
+                opacity: 1
+            }}
+            transition={{
+                type: 'timing',
+                delay: 300, 
+                duration: 900
+            }}
+        >
             <View style={styles.item}>
                 <Text style={styles.itemTitle}>Saldo</Text>
                     <View style={styles.content}>
@@ -19,7 +36,7 @@ export default function Balance({ balance, expenses }) {
                         <Text style={styles.expenses}>{expenses}</Text>
                     </View>
             </View>
-        </View>
+        </MotiView>
     )
 }
 
